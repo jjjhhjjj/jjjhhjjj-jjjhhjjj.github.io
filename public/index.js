6104bb8af8e7d0fecaf98ad480b7b097e9c82cbc
jjjhhjjj <119382361+jjjhhjjj@users.noreply.github.com>
2022-12-19 14:15:20 +0900

    Add files via upload

---
+++ public/index.js
@@ -0,0 +1,121 @@
+const inputBar = document.querySelector("#comment-input");
+const rootDiv = document.querySelector("#comments");
+const btn = document.querySelector("#submit");
+const mainCommentCount = document.querySelector('#count');
+
+function generateTime(){
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const wDate = date.getDate();
+    const hour = date.getHours();
+    const min = date.getMinutes();
+    const sec = date.getSeconds();
+
+    const time = year+'-'+month+'-'+wDate+' '+hour+':'+min+':'+sec;
+    return time;
+
+}
+
+function generateUserName(){
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    var makeUsername = '';
+    for(let i=0; i<4;i++){
+        let index = Math.floor(Math.random(10) * alphabet.length);
+        makeUsername += alphabet[index];        
+    }
+    for(let j=0;j<4;j++){
+        makeUsername += "*";
+    }
+    return makeUsername;    
+}
+
+function numberCount(event){
+    console.log(event.target);
+    if(event.target === voteUp){
+        console.log("2");
+      return voteUp.innerHTML++; 
+      
+    }else if(event.target === voteDown){
+      return voteDown.innerHTML++; 
+    }   
+    
+}
+
+function deleteComments(event){    
+    const btn = event.target;    
+    const list = btn.parentNode.parentNode;
+    rootDiv.removeChild(list);
+    if(mainCommentCount.innerHTML <='0'){
+        mainCommentCount.innerHTML = 0;
+    }else{
+        mainCommentCount.innerHTML--;
+    }
+}
+
+
+function showComment(comment){
+    const userName = document.createElement('div');
+    const inputValue = document.createElement('span');
+    const showTime = document.createElement('div');
+    const voteDiv = document.createElement('div');
+    const countSpan = document.createElement('span')
+    const commentList = document.createElement('div');  
+    const delBtn = document.createElement('button');
+    delBtn.className ="deleteComment";
+    delBtn.innerHTML="삭제";
+    commentList.className = "eachComment";
+    userName.className="name";
+    inputValue.className="inputValue";
+    showTime.className="time";
+    voteDiv.className="voteDiv";
+    userName.innerHTML = generateUserName();    
+    userName.appendChild(delBtn);  
+    inputValue.innerText = comment;
+    showTime.innerHTML = generateTime();
+    countSpan.innerHTML=0;
+     
+    commentList.appendChild(userName);
+    commentList.appendChild(inputValue);
+    commentList.appendChild(showTime);
+    commentList.appendChild(voteDiv);
+    rootDiv.prepend(commentList);
+
+    delBtn.addEventListener("click",deleteComments);
+   console.dir(rootDiv);
+
+}
+
+function pressBtn(){ 
+   const currentVal = inputBar.value;
+   
+   if(!currentVal.length){
+      alert("댓글을 입력해주세요!!");
+   }else{
+      showComment(currentVal);  
+      mainCommentCount.innerHTML++;
+      inputBar.value ='';
+   }
+}
+
+btn.onclick = pressBtn;
+
+var Target = document.getElementById("clock");
+function clock() {
+    var time = new Date();
+    var month = time.getMonth();
+    var date = time.getDate();
+    var day = time.getDay();
+    var week = ['일', '월', '화', '수', '목', '금', '토'];
+
+    var hours = time.getHours();
+    var minutes = time.getMinutes();
+    var seconds = time.getSeconds();
+
+    Target.innerText = 
+    `${month + 1}월 ${date}일 ${week[day]}요일 ` +
+    `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+        
+}
+clock();
+setInterval(clock, 1000);
