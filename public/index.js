a593b7e08ae49b5263d26db0862bacbd9d58e570
jjjhhjjj <119382361+jjjhhjjj@users.noreply.github.com>
2022-12-19 17:28:04 +0900

    Update index.js

--- public/index.js
+++ public/index.js
@@ -88,6 +88,7 @@ btn.onclick = pressBtn;
 var Target = document.getElementById("clock");
 function clock() {
     var time = new Date();
+    var year = time.getFullYear();
     var month = time.getMonth();
     var date = time.getDate();
     var day = time.getDay();
@@ -98,7 +99,7 @@ function clock() {
     var seconds = time.getSeconds();
 
     Target.innerText = 
-    `${month + 1}월 ${date}일 ${week[day]}요일 ` +
+    `${year}년 ${month + 1}월 ${date}일 ${week[day]}요일 ` +
     `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
         
 }
